0568b7e92ca0a7d0ac1b6492f4c483d75985f424
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-02 18:48:20 -0400

    added jsx, updated css, and create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testminoials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
 
 const App = () => {
   return (
@@ -9,6 +17,16 @@ const App = () => {
     <Header />
     <Nav />
     <About/>
+  <Experience/>
+  <Services/>
+  <Portfolio/>
+  <Testminoials/>
+  <Contact/>
+  <Footer/>
+    
+
+  
+    
 
 
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
+import './header.css'
+import me from '../../assets/me.jpeg'
 
-const header = () => {
+const Header = () => {
   return (
-    <div>header</div>
+    <header>
+        <div className="container header__container">
+            <h5>
+                Hello I'm            </h5>
+                <h1> Keenan Kimbrough</h1>
+                <h5 classnName="text-light"> Full Stack Developer </h5>
+                <CTA/>
+
+                <div className="me">
+                    <img src={me} alt=""/>
+                    </div>
+
+                    <a href='#contact' className='scroll_down'> Scroll Down</a>
+        </div>
+        </header>
   )
 }
 
-export default header
+export default Header
