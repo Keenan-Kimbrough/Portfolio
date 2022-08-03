1697997833ee51bb497b11752265d270dda8fe49
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 19:56:46 -0400

    updated portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+        <h5> My Recent Work</h5>
+        <h2> Portfolio</h2>
+
+<div className='container portfolio container'> 
+    <article className="portfolio__item">
+        <div className="portfolio__item-image">
+
+        </div>
+        <h3> This is a portfolio Item title</h3>
+        <a href='http://github.com/keenan-kimbrough' className='btn' target='_blank'> GitHub</a>
+        <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'> Live Demo </a>
+    </article>
+    </div>
+        Portfolio
+
+
+
+    </section>
   )
 }
 
