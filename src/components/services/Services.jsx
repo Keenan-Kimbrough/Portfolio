1cb7b3d4d04dd03d7779322dc603beca41557791
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 19:47:28 -0400

    updated services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,163 @@
 import React from 'react'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+        <h5> What I Offer</h5>
+        <h2> Servicees</h2>
+
+        <div>
+             <article className="services">
+                <div className="services__head">
+                    <h3> UI/UX Desgin </h3>
+                </div>
+                <ul>
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                </ul>
+            </article>
+            {/* End Of UI/ UX */}
+            <article className="services">
+                <div className="services__head">
+                    <h3> UI/UX Desgin </h3>
+                </div>
+                <ul>
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                </ul>
+            </article>
+            {/* End of Web Development*/}
+            <article className="services">
+                <div className="services__head">
+                    <h3> UI/UX Desgin </h3>
+                </div>
+                <ul>
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                    <li>
+                        <BiCheck className="services__list-icon"/>
+                            <p> Lorem, ipsum dolor sit amet consectectur elit.</p>
+                    </li>
+
+                </ul>
+            </article>
+            {/* end of content createion */}
+
+            </div>
+    </section>
   )
 }
 
