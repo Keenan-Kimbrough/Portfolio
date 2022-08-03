c047dd32a84dace438f4c89fccbfce2699031d65
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 18:31:08 -0400

    updated jsx and added css updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import './about.css'
-
+import me from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 
 function About() {
   return (
-    <section id="about">About</section>
+    <section id="about">
+        <h5> Get To KNow</h5>
+        <h2> About Me</h2>
+        <div className="container about__container">
+            <div className="about__me">
+                <div className="about__me-image">
+                     <img src={me} alt="About Image"/>
+       </div>
+                </div>
+            <div className="about__content">
+            <div className='about__cards'>
+                <article className='about__card'>
+                    <FaAward className='about__icon' />
+
+                    <h5> Experience</h5>
+                    <small> 3+ Years Working Experience </small>
+                </article>
+                <article className='about__card'>
+                    <FiUsers className='about__icon' />
+
+                    <h5> Clients</h5>
+                    <small> 200+ Clients World wide </small>
+                </article>
+                <article className='about__card'>
+                    <VscFolderLibrary className='about__icon' />
+                    <h5> Projects</h5>
+                    <small> 3+ Years Working Experience </small>
+                </article> 
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magni officia 
+                hic architecto consequuntur 
+                quos ipsam esse rem vitae dolorem officiis amet, beatae consectetur? 
+                Harum aspernatur voluptates architecto fugiat ut.
+            </p>
+        <a href="#contact" className='btn btn-primary'></a>
+            </div>
+        </div>
+            
+    </section>
   )
 }
 
