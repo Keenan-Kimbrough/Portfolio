6a4fa4f7c44a46fb765d5ace6062708a3809c771
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 23:12:49 -0400

    added pagination to testiominals

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,6 +4,16 @@ import me from '../../assets/me.jpg'
 import Henoak from '../../assets/Henoak.jpeg'
 import Jasmine from '../../assets/Jasmine.jpeg'
 import valentine from '../../assets/valentine.jpeg'
+// import Swiper core and required modules
+import { Pagination, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+
 
 
 const data =[
@@ -28,11 +38,18 @@ const testimonials = () => {
     <section id='testimonials'>
         <h5> Review From Clients</h5>
         <h2> Testimonials</h2>
-        <div className='container testimonials__container'> 
+        <Swiper className='container testimonials__container'
+        // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+      
+      
             {
                 data.map(({avatar, name, review}, index) => {
                   return(  
-                  <article key={index} className="testimonial">
+                  <SwiperSlide key={index} className="testimonial">
                 <div className="client__avatar">
                     <img src={avatar} alt=''/>
                 </div>
@@ -41,10 +58,10 @@ const testimonials = () => {
                             {review}
                        
                         </small>
-            </article>)
+            </SwiperSlide>)
                 })
             }
-        </div>
+        </Swiper>
     </section>
   )
 }
