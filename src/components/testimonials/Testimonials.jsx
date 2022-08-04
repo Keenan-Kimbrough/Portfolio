7bb8f8e61f7b94652445cfcb45e1ede3049123ba
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 21:42:08 -0400

    updated testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './testimonials.css'
 const testimonials = () => {
   return (
-    <section id='testimonials'>testimonials</section>
+    <section id='testimonials'>
+        <h5> Review From Clients</h5>
+        <h2> Testimonials</h2>
+        <div className='container testiomnials__container'> 
+            <article className="testimonials">
+                <div>
+                    <img src="" alt=""/>
+                    <h5 className="client__name"> Jasmine Wallace</h5>
+                        <small className="client__review">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur 
+                        consequatur pariatur quaerat atque nam, veniam totam, dolores quos magnam 
+                        a temporibus sequi nulla. Dolorum maxime explicabo dolore. Voluptates, minima at.
+                        </small>
+                </div>
+            </article>
+        </div>
+    </section>
   )
 }
 
