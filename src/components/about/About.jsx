0383a7b2dfdee7f2b3bfe340707ba76814062807
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-04 01:21:22 -0400

    updated jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ import {VscFolderLibrary} from 'react-icons/vsc'
 function About() {
   return (
     <section id="about">
-        <h5> Get To KNow</h5>
+        <h5> Get To Know</h5>
         <h2> About Me</h2>
         <div className="container about__container">
             <div className="about__me">
@@ -38,10 +38,19 @@ function About() {
                 </article> 
             </div>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magni officia 
-                hic architecto consequuntur 
-                quos ipsam esse rem vitae dolorem officiis amet, beatae consectetur? 
-                Harum aspernatur voluptates architecto fugiat ut.
+            Loving Life!
+
+I've always have been a great problem solver and into technology! As of now I am working remote as a Software Engineer Apprentice at Argo AI. where I have the opportunity to 
+advance my knowledge and understanding of software development.I started learning to code when i was a teenager, at the time I really I didn't even know what coding was.
+I would try to run private servers for games on my computer such as runescape and and update websites of my social media accounts such as myspace. My first official class of
+coding was actually at Penn state Freshman year of college. Not too exactly sure of the class name but I just remember it was difficult at first to understand C++. Overall I didn't
+give up and I kept trying to perfect my craft until I understood it all and could run different scripts. After my education in Mechanical Engineering I decided to pursue my business 
+degree and then I realized how much I enjoyed coding. Therefore I decided to go back to school to attain a bachelors in computer Science from Western Governors University. Overall it
+ took a little while, but I finally realized software development is for me.
+Since then I've worked on countless freelance projects and have also worked with mid level
+ and senior software engineers on projects as well. I'm familiar with a variety of programming 
+ languages and frameworks, including TypeScript, JavaScript,ReactJS, HTML, CSS, Python, DJango, Java,
+  C++, and SQL, but I am always trying my best to add new skills to my repertoire. In addition, I would love to meet other software engineers, so feel free to reach out to me to connect! :)
             </p>
         <a href="#contact" className='btn btn-primary'> Let's Talk</a>
             </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -15,21 +15,21 @@ const data = [
         id: 1,
         image: img1,
         title: '  Black Jack App',
-        github: 'https://github.com',
+        github: 'https://github.com/Keenan-Kimbrough/Black_Jack_React_TypeScript',
         demo: 'https://https://www.forbes.com/sites/bernardmarr/2022/01/05/the-10-tech-trends-that-will-transform-our-world/.com'
     },
     {
         id: 2,
         image: img2,
         title: ' Crypto Currency Tracker App',
-        github: 'https://github.com',
+        github: 'https://github.com/Keenan-Kimbrough/Cryptocurrency_Tracker',
         demo: 'https://https://www.forbes.com/sites/bernardmarr/2022/01/05/the-10-tech-trends-that-will-transform-our-world/.com'
     },
     {
         id: 3,
         image: img3,
         title: 'Ecommerce Website',
-        github: 'https://github.com',
+        github: 'https://github.com/Keenan-Kimbrough/Clothing-Store-ReactJS',
         demo: 'https://https://www.forbes.com/sites/bernardmarr/2022/01/05/the-10-tech-trends-that-will-transform-our-world/.com'
     },
     {
@@ -50,7 +50,7 @@ const data = [
         id: 6,
         image: img6,
         title: ' Social Media App',
-        github: 'https://github.com',
+        github: 'https://github.com/Keenan-Kimbrough/Twitter-App',
         demo: 'https://github.com'
     }
 ]
