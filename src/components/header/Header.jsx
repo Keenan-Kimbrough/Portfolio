8d1c600c61d43564566846979542320395c63f9c
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-04 01:05:36 -0400

    updated personal picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CTA from './CTA'
 import './header.css'
-import Keenan from '../../assets/Keenan.jpeg'
+import Keenan from '../../assets/Keenan.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
