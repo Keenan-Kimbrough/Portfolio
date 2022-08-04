edc469ac5723864e6173560ebf5d21494b2e6496
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-08-03 21:54:50 -0400

    updated testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import './testimonials.css'
+import me from '../../assets/me.jpg'
 const testimonials = () => {
   return (
     <section id='testimonials'>
         <h5> Review From Clients</h5>
         <h2> Testimonials</h2>
-        <div className='container testiomnials__container'> 
-            <article className="testimonials">
+        <div className='container testimonials__container'> 
+            <article className="testimonial">
                 <div>
                     <img src="" alt=""/>
-                    <h5 className="client__name"> Jasmine Wallace</h5>
+                </div>
+                <h5 className="client__name"> Jasmine Wallace</h5>
                         <small className="client__review">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur 
                         consequatur pariatur quaerat atque nam, veniam totam, dolores quos magnam 
                         a temporibus sequi nulla. Dolorum maxime explicabo dolore. Voluptates, minima at.
                         </small>
-                </div>
             </article>
         </div>
     </section>
